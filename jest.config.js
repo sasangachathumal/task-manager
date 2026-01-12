1f9ea2754d5d3521ed5ecd8c2daea616720f3c62
Sasanga Chathumal <chathumal.sasanga02@gmail.com>
2026-01-12 20:52:26 +0530

    config jest to run unit tests.

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  // This line is crucial for modern Angular (ESM)
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+};
